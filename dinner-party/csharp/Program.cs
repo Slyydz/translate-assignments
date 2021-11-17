c6824b01809aea6a6fba62e27a4d03cf7ea29d49
Brady Williams <williams.cole29@yahoo.com>
2021-11-17 15:24:13 -0600

    UNfinished

---
+++ dinner-party/csharp/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Guests> guests = getGuests();
+
+
+            List<Guests> table1 = new List<Guests>();
+            List<Guests> table2 = new List<Guests>();
+
+
+
+
+
+
+            foreach (var guest in guests)
+            {
+                // Console.WriteLine(guest.Occupation);
+                table1.Add(guest);
+                if (table1.Contains(guest.Occupation.ToString()))
+                {
+                    table2.Add(guest);
+                }
+                else
+                {
+                    table1.Add(guest);
+                }
+            }
+
+            foreach (var item in table1)
+            {
+                Console.WriteLine(item.Name);
+            }
+            foreach (var item in table2)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+
+
+            List<Guests> getGuests()
+            {
+                List<Guests> retList = new List<Guests>(){
+
+                new Guests{
+      Name = "Marilyn Monroe",
+      Occupation = "entertainer",
+      Bio = "(1926 - 1962) American actress, singer, model"
+    },
+
+            new Guests{
+      Name = "Abraham Lincoln",
+      Occupation = "politician",
+      Bio = "(1809 - 1865) US President during American civil war"
+    }
+
+            };
+                return retList;
+
+            }
+        }
+    }
+
+    public class Guests
+    {
+        public string Name { get; set; }
+
+        public string Occupation { get; set; }
+
+        public string Bio { get; set; }
+
+    }
+
+
+}
